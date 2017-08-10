fbd4e5ade03951bd7e0ab48dd416fd95f1eadb42
Josh Curl <josh@curl.me>
2017-08-09 20:23:13 -0700

    Test CLI and service order

--- main.go
+++ main.go
@@ -1,55 +1,17 @@
 package main
 
 import (
-	"context"
-	"io/ioutil"
 	"os"
+	"path"
 
-	"github.com/rancher/go-rancher/v3"
-	"github.com/rancher/rancher-compose-executor/project"
-	"github.com/rancher/rancher-compose-executor/project/options"
-	_ "github.com/rancher/rancher-compose-executor/resources"
-)
-
-const (
-	cattleURLEnv       = "RANCHER_URL"
-	cattleAccessKeyEnv = "RANCHER_ACCESS_KEY"
-	cattleSecretKeyEnv = "RANCHER_SECRET_KEY"
+	"github.com/rancher/rancher-compose-executor/executor"
+	"github.com/rancher/rancher-compose-executor/testcli"
 )
 
 func main() {
-	//executor.Main()
-
-	b, err := ioutil.ReadFile("compose.yml")
-	if err != nil {
-		panic(err)
+	if path.Base(os.Args[0]) == "rancher-compose-executor" {
+		executor.Main()
+	} else {
+		testcli.Main()
 	}
-
-	rancherClient, err := createRancherClient()
-	if err != nil {
-		panic(err)
-	}
-
-	p := project.NewProject("s", rancherClient)
-	if err := p.Load(map[string]interface{}{
-		"compose.yml": b,
-	}, map[string]string{}); err != nil {
-		panic(err)
-	}
-
-	if err := p.Create(context.Background(), options.Options{}); err != nil {
-		panic(err)
-	}
-}
-
-func createRancherClient() (*client.RancherClient, error) {
-	url, err := client.NormalizeUrl(os.Getenv(cattleURLEnv))
-	if err != nil {
-		return nil, err
-	}
-	return client.NewRancherClient(&client.ClientOpts{
-		Url:       url,
-		AccessKey: os.Getenv(cattleAccessKeyEnv),
-		SecretKey: os.Getenv(cattleSecretKeyEnv),
-	})
 }
